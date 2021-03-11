e0e6eb3c3f712f3454c58a351976379311e8c931
cjreardon <connorreardon3@gmail.com>
2021-03-11 11:24:40 -0500

    some error checking and switched to a 2d list to represent squares

--- play.py
+++ play.py
@@ -1,97 +1,125 @@
-def print_board():
+def print_board(arr):
     print("     |     |     ")
-    print(str1)
+    print("  "+arr[0][0]+"  |  "+arr[0][1]+"  |  "+arr[0][2]+"   ")
     print("_____|_____|_____")
     print("     |     |     ")
-    print(str2)
+    print("  "+arr[1][0]+"  |  "+arr[1][1]+"  |  "+arr[1][2]+"   ")
     print("_____|_____|_____")
     print("     |     |     ")
-    print(str3)
+    print("  "+arr[2][0]+"  |  "+arr[2][1]+"  |  "+arr[2][2]+"   ")
     print("     |     |     ")
 
 def is_over(arr):
-    if arr[0] == "X" and arr[1] == "X" and arr[2] == "X":
+    if arr[0][0] == "X" and arr[0][1] == "X" and arr[0][2] == "X":
         print("Player 1 wins!!!\n")
         return True
-    if arr[0] == "X" and arr[4] == "X" and arr[8] == "X":
+    if arr[0][0] == "X" and arr[1][1] == "X" and arr[2][2] == "X":
         print("Player 1 wins!!!\n")
         return True
-    if arr[0] == "X" and arr[3] == "X" and arr[6] == "X":
+    if arr[0][0] == "X" and arr[1][0] == "X" and arr[2][0] == "X":
         print("Player 1 wins!!!\n")
         return True
-    if arr[3] == "X" and arr[4] == "X" and arr[5] == "X":
+    if arr[1][0] == "X" and arr[1][1] == "X" and arr[1][2] == "X":
         print("Player 1 wins!!!\n")
         return True
-    if arr[6] == "X" and arr[7] == "X" and arr[8] == "X":
+    if arr[2][0] == "X" and arr[2][1] == "X" and arr[2][2] == "X":
         print("Player 1 wins!!!\n")
         return True    
-    if arr[1] == "X" and arr[4] == "X" and arr[7] == "X":
+    if arr[0][1] == "X" and arr[1][1] == "X" and arr[2][1] == "X":
         print("Player 1 wins!!!\n")
         return True  
-    if arr[2] == "X" and arr[5] == "X" and arr[8] == "X":
+    if arr[0][2] == "X" and arr[1][2] == "X" and arr[2][2] == "X":
         print("Player 1 wins!!!\n")
         return True    
-    if arr[6] == "X" and arr[4] == "X" and arr[2] == "X":
+    if arr[2][0] == "X" and arr[1][1] == "X" and arr[0][2] == "X":
         print("Player 1 wins!!!\n")
         return True    
-    if arr[0] == "O" and arr[1] == "O" and arr[2] == "O":
+    if arr[0][0] == "O" and arr[0][1] == "O" and arr[0][2] == "O":
         print("Player 2 wins!!!\n")
         return True
-    if arr[0] == "O" and arr[4] == "O" and arr[8] == "O":
+    if arr[0][0] == "O" and arr[1][1] == "O" and arr[2][2] == "O":
         print("Player 2 wins!!!\n")
         return True
-    if arr[0] == "O" and arr[3] == "O" and arr[6] == "O":
+    if arr[0][0] == "O" and arr[1][0] == "O" and arr[2][0] == "O":
         print("Player 2 wins!!!\n")
         return True
-    if arr[3] == "O" and arr[4] == "O" and arr[5] == "O":
+    if arr[1][0] == "O" and arr[1][1] == "O" and arr[1][2] == "O":
         print("Player 2 wins!!!\n")
         return True
-    if arr[6] == "O" and arr[7] == "O" and arr[8] == "O":
+    if arr[2][0] == "O" and arr[2][1] == "O" and arr[2][2] == "O":
         print("Player 2 wins!!!\n")
         return True    
-    if arr[1] == "O" and arr[4] == "O" and arr[7] == "O":
+    if arr[0][1] == "O" and arr[1][1] == "O" and arr[2][1] == "O":
         print("Player 2 wins!!!\n")
         return True  
-    if arr[2] == "O" and arr[5] == "O" and arr[8] == "O":
+    if arr[0][2] == "O" and arr[1][2] == "O" and arr[2][2] == "O":
         print("Player 2 wins!!!\n")
         return True    
-    if arr[6] == "O" and arr[4] == "O" and arr[2] == "O":
+    if arr[2][0] == "O" and arr[1][1] == "O" and arr[0][2] == "O":
         print("Player 2 wins!!!\n")
         return True   
     else:
         return False
 
-arr = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-turn = 0
-str1 = "  1  |  2  |  3  "
-str2 = "  4  |  5  |  6  "
-str3 = "  7  |  8  |  9  "
+def get_square(index):
+    if index == "1":
+        row = 0
+        col = 0
+    if index == "2":
+        row = 0
+        col = 1
+    if index == "3":
+        row = 0
+        col = 2
+    if index == "4":
+        row = 1
+        col = 0
+    if index == "5":
+        row = 1
+        col = 1
+    if index == "6":
+        row = 1
+        col = 2
+    if index == "7":
+        row = 2
+        col = 0
+    if index == "8":
+        row = 2
+        col = 1
+    if index == "9":
+        row = 2
+        col = 2
+    return row, col
 
-while not is_over(arr):
-    turn += 1
-    print_board()
-    if turn % 2 == 1:
-        index = input("Player 1: Where would you like to place your X? ")
-        print()
-        index = int(index)
-        arr[index-1] = "X"
-        if 0<index<4:
-            str1 = str1.replace(str(index), "X")
-        if 3<index<7:
-            str2 = str2.replace(str(index), "X")
-        if 6<index<10:
-            str3 = str3.replace(str(index), "X")
-    else:
-        index = input("Player 2: Where would you like to place your O? ")
-        print()
-        index = int(index)
-        arr[index-1] = "O"
-        if 0<index<4:
-            str1 = str1.replace(str(index), "O")
-        if 3<index<7:
-            str2 = str2.replace(str(index), "O")
-        if 6<index<10:
-            str3 = str3.replace(str(index), "O")
+def two_player():
+    turn = 0
+    arr = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+    while not is_over(arr):
+        turn += 1
+        print_board(arr)
+        if turn % 2 == 1:
+            index = input("Player 1: Where would you like to place your X? ")
+            print()
+            row, col = get_square(index)
+            if arr[row][col] == "X" or arr[row][col] == "O":
+                index = input("Player 1: Enter a square that is not taken. ")
+                print()
+                row, col = get_square(index)
+            arr[row][col] = "X"
+        else:
+            index = input("Player 2: Where would you like to place your O? ")
+            print()
+            row, col = get_square(index)
+            if arr[row][col] == "X" or arr[row][col] == "O":
+                index = input("Player 2: Enter a square that is not taken. ")
+                print()
+                row, col = get_square(index)
+            arr[row][col] = "O"
+    print()
+    print_board(arr)
 
-print()
-print_board()
+game_mode = input("Would you like to play against a human (1) or a computer (2)? ")
+if int(game_mode) == 1:
+    two_player()
+
+
