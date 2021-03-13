5f69e438967f0401b89de8c45128c46a27fc45dc
cjreardon <connorreardon3@gmail.com>
2021-03-13 10:52:04 -0500

    input checking and computer prioritizes winning over blocking

--- play.py
+++ play.py
@@ -21,15 +21,15 @@ def winner(char):
 def could_win(arr, char):
     if arr[0] == char and arr[1] == char and arr[2] == char:
         return True
-    if arr[0] == char and arr[4] == char and arr[8] == char: 
+    if arr[0] == char and arr[4] == char and arr[8] == char:
         return True
-    if arr[0] == char and arr[3] == char and arr[6] == char: 
+    if arr[0] == char and arr[3] == char and arr[6] == char:
         return True
     if arr[3] == char and arr[4] == char and arr[5] == char:
         return True
     if arr[6] == char and arr[7] == char and arr[8] == char:
         return True
-    if arr[1] == char and arr[4] == char and arr[7] == char: 
+    if arr[1] == char and arr[4] == char and arr[7] == char:
         return True
     if arr[2] == char and arr[5] == char and arr[8] == char:
         return True
@@ -55,7 +55,7 @@ def is_over(arr, char):
     if arr[6] == char and arr[7] == char and arr[8] == char:
         winner(char)
         return True
-    if arr[1] == char and arr[4] == char and arr[7] == char: 
+    if arr[1] == char and arr[4] == char and arr[7] == char:
         winner(char)
         return True
     if arr[2] == char and arr[5] == char and arr[8] == char:
@@ -71,14 +71,6 @@ def is_over(arr, char):
         return False
 
 
-def is_taken(arr,i):
-    while arr[i-1] == "X" or arr[i-1] == "O":
-        i = input("Enter a square that is not taken. ")
-        i = int(i)
-    print()
-    return i
-
-
 def two_player():
     turn = 0
     arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -87,15 +79,17 @@ def two_player():
         print_board(arr)
         if turn % 2 == 1:
             index = input("Player 1: Where would you like to place your X? ")
+            while index not in arr or index == "X" or index == "O":
+                index = input("Please enter a valid move ")
             print()
             index = int(index)
-            index = is_taken(arr, index)
             arr[index-1] = "X"
         else:
             index = input("Player 2: Where would you like to place your O? ")
+            while index not in arr or index == "X" or index == "O":
+                index = input("Please enter a valid move ")
             print()
             index = int(index)
-            index = is_taken(arr, index)
             arr[index-1] = "O"
     print()
     print_board(arr)
@@ -109,22 +103,27 @@ def one_player():
         print_board(arr)
         if turn % 2 == 1:
             index = input("Player 1: Where would you like to place your X? ")
+            while index not in arr or index == "X" or index == "O":
+                index = input("Please enter a valid move ")
             print()
             index = int(index)
-            index = is_taken(arr, index)
             arr[index-1] = "X"
         else:
             print()
-            arr[computer_move(arr)] = "O"
+            comp = computer_move(arr)
+            arr[comp] = "O"
+            comp = comp + 1
+            print("Computer placed an O at " + str(comp))
     print()
     print_board(arr)
 
 
 def computer_move(arr):
-    move_bank = [x for x, char in enumerate(arr) if char != 'X' and char != "O"]
+    move_bank = [x for x, char in enumerate(
+        arr) if char != 'X' and char != "O"]
     if 4 in move_bank:
         return 4
-    for char in ["X", "O"]:
+    for char in ["O", "X"]:
         for i in move_bank:
             arr_copy = arr[:]
             arr_copy[i] = char
@@ -132,26 +131,29 @@ def computer_move(arr):
                 return i
     corners = []
     for i in move_bank:
-        if i in [0,2,6,8]:
+        if i in [0, 2, 6, 8]:
             corners.append(i)
-    if len(corners)>0:
+    if len(corners) > 0:
         return corners[0]
     edges = []
     for i in move_bank:
-        if i in [1,3,5,7]:
+        if i in [1, 3, 5, 7]:
             edges.append(i)
-    if len(edges)>0:
+    if len(edges) > 0:
         return edges[0]
-
 
 
 def main():
     game_mode = input(
         "Would you like to play against a human (1) or a computer (2)? ")
+    while(game_mode != "1" and game_mode != "2"):
+        game_mode = input("Please enter 1 or 2 ")
     if int(game_mode) == 1:
         two_player()
     elif int(game_mode) == 2:
+        print("Good Luck ;)")
         one_player()
+
 
 main()
 
